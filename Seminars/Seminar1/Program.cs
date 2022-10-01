c28ecccace08c4811156a1a3b62f82757b28afd7
Andrey Sorokin <ands@list.ru>
2022-10-01 21:01:41 +0400

    first seminar two task complete

---
+++ Seminars/Seminar1/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+
+// Задача 1. Текст задачи.
+
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = b*b;
+
+if (a==quad2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+/*
+
+// Задача 2.
+
+Console.Write("Input a number = ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int current=-number;
+
+if(number>0)
+{
+    current=-number;
+}
+else
+{
+    current=number;
+    number*=-1;
+}
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+
+*/
+
+/*
+// Выделение цифр из числа:
+
+253 % 10 -> 3
+253 % 100 -> 53
+253 % 1000 -> 253
+
+253 / 10 -> 25
+253 / 100 -> 2
+253 / 1000 -> 0
+
+4827 % 1000 /10 ->82
+4827 / 10 % 100 ->82
+
+*/
+
