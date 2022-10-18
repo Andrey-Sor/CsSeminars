7cd59d66c79d927a47b29bd43a93b8762b303ee9
Andrey Sorokin <ands@list.ru>
2022-10-18 05:29:47 +0400

    Completed homework of the fourth seminar

---
+++ Homeworks/HomeWork04/Program.cs
@@ -0,0 +1,97 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+/*
+int DegreeNumber(int num, int degr)
+{
+    int result = 1;
+        
+    for(int current = 1; current <= degr; current++)    
+        result *= num;
+    return result;
+
+    // int current = 1;
+    // while(current <= degr)
+    // {
+    //     result *= num;
+    //     current++;
+    // }
+    // return result;
+
+}
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input degree number: ");
+int degr = Convert.ToInt32(Console.ReadLine());
+
+int res = DegreeNumber(num, degr);
+
+Console.WriteLine($"Degree {degr} of number {num} is {res}");
+*/
+
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+/*
+int DigitSum(int n)
+{
+    int result = n;
+    int sum = 0;
+       
+    while(result >=1)
+    {
+        sum = sum + result % 10;
+        result = result /10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Input positive integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int d = DigitSum(number);
+
+Console.WriteLine($"Sum digits of numbers from {number} is {d}");
+*/
+
+
+/*
+Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+m = 5 -> [1, 2, 5, 7, 19]
+m = 3 -> [6, 1, 33]
+*/
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+   
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+Console.WriteLine("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input minimum element of array: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input maximum element of array: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+*/
---
+++ Seminars/Seminar4/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+/*
+int GetSum(int a)
+{
+    int sum = 0;
+    
+    for(int current = 1; current <= a; current++)    
+        sum += current;
+    return sum;
+}
+
+Console.WriteLine("Input positive integer number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int s = GetSum(num);
+
+Console.WriteLine($"Sum of numbers from 1 to  {num} is {s}");
+*/
+
+// Задача: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+/*
+int DigitNumber(int n)
+{
+    int count = 1;
+
+    for(int div = n; div >= 10; count++)
+        div = div / 10;
+
+    return count;
+}
+
+Console.WriteLine("Input positive integer number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int d = DigitNumber(num);
+
+Console.WriteLine($"Quantity of numbers from {num} is {d}");
+
+
+
+// Задача: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+/*
+int GetMult(int a)
+{
+    int mult = 1;
+    
+    for(int current = 1; current <= a; current++)    
+        mult *= current;
+    return mult;
+}
+
+Console.WriteLine("Input positive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int m = GetMult(num);
+
+Console.WriteLine($"Multiplication of numbers from 1 to  {num} is {m}");
+*/
+
+// Задача 4: Напишите программу, которая выводит массив из m элементов, заполненный нулями и единицами в случайном порядке.
+
+// метод генерации массива:
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] newArray = new int[size];
+
+    for(int i = 0; i < size; i++)
+        newArray[i] = new Random().Next(minValue, maxValue + 1);
+
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+Console.WriteLine("Input a number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 9;
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
