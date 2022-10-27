d4f5c70e544f385e36ec5e744b6bc67836e5e388
Andrey Sorokin <ands@list.ru>
2022-10-28 02:25:56 +0400

    Create folder Tasks/Tasks01 and solve tasks

---
+++ Homeworks/HomeWork06/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int[] CreatArray()
+{
+    Console.Write("How many numbers do you want to enter? - ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input a {i+1} number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        array[i] = num;
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int FindPositiveNumbers(int[] userArray)
+{
+    int count = 0;
+    for(int i = 0; i < userArray.Length; i++)
+    {
+        if(userArray[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+int[] userArray = CreatArray();
+Console.Write("\nYour numbers: ");
+ShowArray(userArray);
+
+int findPositiveNum = FindPositiveNumbers(userArray);
+Console.WriteLine($"\nQuantity of positive numbers : {findPositiveNum}\n");
---
+++ Tasks/Tasks01/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача № 1
+Необходимо написать консольную программу, где пользователь будет вводить с клавиатуры 2 числа. Числа будут сравниваться с последующим выводом в консоль
+результата этого сравнения (равны ли значения, а если нет, то какое число больше/меньше).
+*/
+
+/*
+void CompaireNumbers(int a, int b)
+{
+    if(a == b)
+    {
+        Console.WriteLine($"Numder {a} = {b}");
+    }
+    else
+    {
+        if(a > b)
+            Console.WriteLine($"Number {a} > {b}");
+        else
+            Console.WriteLine($"Number {a} < {b}");
+    }
+}
+
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+CompaireNumbers(a, b);
+*/
+
+/*
+Пусть пользователь имеет возможность ввести одно число, причем:
+— когда значение больше пяти и одновременно с этим меньше десяти, выводится один текст с соответствующим сообщением;
+—  когда значение не попадает в указанный диапазон, программа его не узнает, выводя другой текст с соответствующим сообщением. Таким образом, алгоритм решения будет несколько отличаться:
+*/
+/*
+void Recognize(int num)
+{
+    if(num > 5 && num < 10)
+        Console.WriteLine("This number is right!");
+    else
+        Console.WriteLine("This number is not right");
+
+}
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Recognize(num);
+*/
